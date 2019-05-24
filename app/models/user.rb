486c391a2b4155593f051f409021d073c2ab536d
Edmhar Laoag <edmhar@ip-192-168-12-56.ec2.internal>
2019-05-24 18:04:59 +0800

    Working login system

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User
+  include Mongoid::Document
+  include ActiveModel::SecurePassword
+
+  field :email, type: String
+  field :name, type: String
+  field :role, type: String
+  field :password_digest
+
+  has_secure_password
+
+end
